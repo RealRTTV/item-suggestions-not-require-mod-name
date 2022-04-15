f2b0c56f6753b15ad8765454bf604d393d3f1d42
RTTV <tristanandlucky@gmail.com>
2022-04-15 09:12:00 -0400

    changed to a redirect cause its neater and probably removes a log message that is annoying :/

--- src/main/java/ca/rttv/isnrmn/mixin/CommandSourceMixin.java
+++ src/main/java/ca/rttv/isnrmn/mixin/CommandSourceMixin.java
@@ -1,39 +1,14 @@
 package ca.rttv.isnrmn.mixin;
 
 import net.minecraft.command.CommandSource;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
-
-import java.util.function.Consumer;
-import java.util.function.Function;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(CommandSource.class)
 public interface CommandSourceMixin {
-    @Shadow
-    static boolean method_27136(String string, String string2) {return false;}
-
-    /**
-     * @reason cuz im lazy :p, also no-one uses this method so yay
-     * @author RTTV
-     */
-    @Overwrite
-    static <T> void forEachMatching(Iterable<T> candidates, String string, Function<T, Identifier> identifier, Consumer<T> action) {
-        boolean bl = string.indexOf(58) > -1;
-
-        for(T object : candidates) {
-            Identifier identifier2 = identifier.apply(object);
-            if (bl) {
-                String string2 = identifier2.toString();
-                if (method_27136(string, string2)) {
-                    action.accept(object);
-                }
-            } else if (method_27136(string, identifier2.getNamespace())
-                    || method_27136(string, identifier2.getPath())) {
-                action.accept(object);
-            }
-        }
-
+    @Redirect(method = "forEachMatching(Ljava/lang/Iterable;Ljava/lang/String;Ljava/util/function/Function;Ljava/util/function/Consumer;)V", at = @At(value = "INVOKE", target = "Ljava/lang/String;equals(Ljava/lang/Object;)Z"))
+    private static boolean equals(String instance, Object anObject) {
+        return true;
     }
 }
